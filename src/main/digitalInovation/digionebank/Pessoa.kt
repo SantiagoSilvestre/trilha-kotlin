6aa5c8b5cdaf3f9e7e137a633817f6ce90ea15f1
Santiago Silvestre <silvestre.santiago732@gmail.com>
2021-03-24 22:38:58 -0300

    Update to apply the private modifier to set function of cpf propert

--- src/main/digitalInovation/digionebank/Pessoa.kt
+++ src/main/digitalInovation/digionebank/Pessoa.kt
@@ -4,17 +4,13 @@ class Pessoa {
 
     var nome: String = "Santiago"
     var cpf: String = "123.123.123-11"
+    private set
 
-
-    inner class Endereco {
-        var rua: String = "Rua teste"
-    }
 }
 
 fun main () {
     val santiago = Pessoa ()
-
+    println(santiago)
     println(santiago.nome)
     println(santiago.cpf)
-    println(santiago.Endereco().rua)
 }
