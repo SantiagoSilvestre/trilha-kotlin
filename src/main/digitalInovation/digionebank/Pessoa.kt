9b0786fcb0aebdfc06e74c289ee1e2b058bd41eb
Santiago Silvestre <silvestre.santiago732@gmail.com>
2021-03-24 22:51:16 -0300

    Update to talk about class members

--- src/main/digitalInovation/digionebank/Pessoa.kt
+++ src/main/digitalInovation/digionebank/Pessoa.kt
@@ -4,15 +4,16 @@ class Pessoa {
 
     var nome: String = "Santiago"
 
-
     var cpf: String = "123.123.123-11"
     private set
+
+    constructor()
+
+    fun pessoaInfo () = "$nome e $cpf"
 
 }
 
 fun main () {
     val santiago = Pessoa ()
-    println(santiago)
-    println(santiago.nome)
-    println(santiago.cpf)
+    println(santiago.pessoaInfo())
 }
