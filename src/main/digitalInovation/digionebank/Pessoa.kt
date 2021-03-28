bb92da4666948ede5b9b48cecc69cf0b05863155
Santiago Silvestre <silvestre.santiago732@gmail.com>
2021-03-28 00:46:53 -0300

    updated to make person and employer abstract class

--- src/main/digitalInovation/digionebank/Pessoa.kt
+++ src/main/digitalInovation/digionebank/Pessoa.kt
@@ -1,19 +1,8 @@
 package main.digitalInovation.digionebank
 
-class Pessoa {
+abstract class Pessoa (
+    val nome: String,
+    val cpf: String
+        ) {
 
-    var nome: String = "Santiago"
-
-    var cpf: String = "123.123.123-11"
-    private set
-
-    constructor()
-
-    fun pessoaInfo () = "$nome e $cpf"
-
-}
-
-fun main () {
-    val santiago = Pessoa ()
-    println(santiago.pessoaInfo())
 }
